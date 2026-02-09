07ea257f81eaea0e2183786153fdfd79e0a37210
Zakir <zakir.mbstu.ict07@gmail.com>
2026-02-09 18:19:37 +0800

    Grant S3 read permission to CodeBuild's role

--- infra/CodePipeline/Example-1/iam-policy.tf
+++ infra/CodePipeline/Example-1/iam-policy.tf
@@ -13,6 +13,7 @@ data "aws_iam_policy_document" "codebuild_policy" {
   statement {
     actions = [
       "s3:PutObject",
+      "s3:GetObject",
       "s3:AbortMultipartUpload",
       "s3:ListBucket",
       "s3:GetBucketLocation"
