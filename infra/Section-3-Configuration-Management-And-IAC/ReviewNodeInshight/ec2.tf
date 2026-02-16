cd71edd7749062c533fb971503f005399e0401b9
Zakir <zakir.mbstu.ict07@gmail.com>
2026-02-16 18:24:54 +0800

    Update EC2 instance resource to support multiple instances with dynamic naming

--- infra/Section-3-Configuration-Management-And-IAC/ReviewNodeInshight/ec2.tf
+++ infra/Section-3-Configuration-Management-And-IAC/ReviewNodeInshight/ec2.tf
@@ -1,7 +1,8 @@
-resource "aws_instance" "dev" {
+resource "aws_instance" "app" {
   ami           = var.ec2_ami_id
   instance_type = var.ec2_instance_type
-  tags          = {
-    Name = "${var.project_name}-app"
+  tags = {
+    Name = "${var.project_name}-app-${count.index + 1}"
   }
+  count = 2
 }
