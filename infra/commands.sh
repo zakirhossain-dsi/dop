0b47cb0e1e50e4496b1701a2db3a6cbf2985c50d
Zakir <zakir.mbstu.ict07@gmail.com>
2026-02-17 10:55:43 +0800

    Add command to retrieve SSM parameters for payments app

--- infra/commands.sh
+++ infra/commands.sh
@@ -3,3 +3,5 @@ systemctl status amazon-ssm-agent
 aws ssm start-session --target i-0d5ed4a60ef98bc09 \
 --profile=terraform-admin \
 --region=ap-southeast-1
+
+aws ssm get-parameters-by-path --path "/payments-app" --recursive
