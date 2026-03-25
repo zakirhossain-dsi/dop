e4d7bb98f0091b987029d4e2fdc637c22f838cc2
Zakir <zakir.mbstu.ict07@gmail.com>
2026-03-25 21:32:24 +0800

    Add Terraform configuration for AWS Organizations and IAM policies

---
+++ infra/Section-7-Security-and-Complience/AWS-Organization/organization.tf
@@ -0,0 +1,31 @@
+resource "aws_organizations_organization" "org" {
+  #    aws_service_access_principals = ["cloudtrail.amazonaws.com"]
+  feature_set = "ALL"
+  enabled_policy_types = [
+    "SERVICE_CONTROL_POLICY",
+  ]
+}
+
+resource "aws_organizations_account" "sandbox" {
+  parent_id = aws_organizations_organization.org.roots[0].id
+  email     = "zakir.mbstu.ict07+sandbox@gmail.com"
+  name      = "Sandbox Account"
+}
+
+data "aws_iam_policy_document" "deny_s3_access_doc" {
+  statement {
+    effect    = "Deny"
+    actions   = ["s3:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_organizations_policy" "deny_s3_access" {
+  name    = "DenyS3Access"
+  content = data.aws_iam_policy_document.deny_s3_access_doc.json
+}
+
+resource "aws_organizations_policy_attachment" "deny_s3_access_attachment" {
+  policy_id = aws_organizations_policy.deny_s3_access.id
+  target_id = aws_organizations_account.sandbox.id
+}
